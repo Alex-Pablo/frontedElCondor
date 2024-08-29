f0171e124fbe3c9eca5c10b3f2b108aca799195f
rtzepi <rtzepi@miumg.edu.gt>
2024-08-28 21:41:30 -0600

    login

--- src/app/core/auth/auth.routes.ts
+++ src/app/core/auth/auth.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from "@angular/router";
 import { PublicLayoutComponent } from "../layout/public-layout/public-layout.component";
-import { LoginComponent } from "./pages/login/login.component";
+import { LoginPageComponent } from './pages/login/login.component';
 import { RegisteruserComponent } from "./pages/registeruser/registeruser.component";
 
 export const AUTH_ROUTES : Routes= [
     {
         path: 'IniciarSesion', component: PublicLayoutComponent, children:[
             {
-                path: '', component: LoginComponent
+                path: '', component: LoginPageComponent
             }
         ]
     },
