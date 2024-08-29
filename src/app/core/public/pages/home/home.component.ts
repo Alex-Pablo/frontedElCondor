997a0ca3b429134924374b5083f2eb5fc0c2eb7f
Angeles <jcuyq@miumg.edu.gt>
2024-08-29 12:37:25 -0600

    sdcfsd

--- src/app/core/public/pages/home/home.component.ts
+++ src/app/core/public/pages/home/home.component.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  NombreEmpresa: string = 'EL CONDOR';
+  Descripcion: string = 'Todo lo que necesitas para construir tus sueños, con herramientas de confianza de la mas alta calidad.';
+  Boton: string ='Ver productos';
+  Subtitulo: string = 'Lista de productos'
+  Categoria: string = 'Categorias';
+  Cat1: string = 'Todas';
+  Cat2: string = 'Carpintería';
+  Cat3: string = 'Albañilería';
+  Cat4: string = 'Categoría X';
+  Cat5: string = 'Categoría R';
+  Herramienta: string = 'Cuchara de albañil';
+  precioHerr: string = 'Q23';
+  Existencia: string = '34';
+  Cod: string = '#323435';
 }
+
+
+
+
