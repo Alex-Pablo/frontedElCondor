a3d95699853764f64820e6d4917bf09468069c8a
ajanels <cajanela1@miumg.edu.gt>
2024-08-30 20:30:05 -0600

    registro de usuarios

--- src/app/features/admin/pages/users/users.component.ts
+++ src/app/features/admin/pages/users/users.component.ts
@@ -5,12 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
-  crearUsuario(){
-    console.log('abrir modal o dialog para crear el usarios');
-  }
 
-  NombreEmpresa: string = 'EL CONDOR';
+export class UsersComponent {
+  //datos para la tabla
+  users = [
+    {
+      no: '#1',
+      foto: 'path_to_user_image',
+      nombre: 'Edwin Alvarez',
+      role: 'Administrador',
+      ultimaActividad: '26/8/2024 3:23',
+      estado: 'activo',
+      creacion: '26/8/2024 10:23',
+    },
+    // Más usuarios
+  ];
 }
+
