77be6b4ae198a7ed4259a2648f3ec0e6894254f8
wreners <wrobless@miumg.edu.gt>
2024-08-30 21:35:56 -0600

    main layout first commit

--- src/app/core/layout/main-layout/main-layout.component.ts
+++ src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
 export class MainLayoutComponent {
+  isConfigOpen = false;
 
+  toggleConfig() {
+    this.isConfigOpen = !this.isConfigOpen;
+  }
 }
