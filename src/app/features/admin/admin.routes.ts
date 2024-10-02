d0fb506b74a893bee650ad83611957f783f14410
Alex-Pablo <alexpabloanibal@gmail.com>
2024-10-01 23:54:40 -0600

    add negocioComponent

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from "@angular/router";
 import { MainLayoutComponent } from "../../core/layout/main-layout/main-layout.component";
 import { UsersComponent } from "./pages/users/users.component";
+import { EmpresaComponent } from "./pages/empresa/empresa.component";
 
 export const ADMIN_ROUTES: Routes = [
   { path: 'usuarios', component: UsersComponent },
+  {
+    path: 'empresa', component: EmpresaComponent
+  },
   {
     path: '', redirectTo: 'usuarios', pathMatch: 'full'
   }
