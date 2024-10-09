0fa35c6fe31814abfdd3cee9f7f9893f9a172776
Angeles <jcuyq@miumg.edu.gt>
2024-10-08 20:24:43 -0600

    cambios

--- src/app/features/catalogo/components/modals/proveedor-popup/proveedor-popup.component.ts
+++ src/app/features/catalogo/components/modals/proveedor-popup/proveedor-popup.component.ts
@@ -1,12 +1,132 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { AuthService } from '../../../../../core/services/auth.service';
+import { SweealertService } from '../../../../../core/services/sweealert.service';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IRole } from '../../../../../shared/models/IRole';
+import { IResult } from '../../../../../shared/models/IResult';
+import { InputFieldComponent } from "../../../../../shared/components/input-field/input-field.component";
+import { InputSelectComponent } from "../../../../../shared/components/input-select/input-select.component";
+import { BtnCloseComponent } from "../../../../../shared/components/btn-close/btn-close.component";
+import { BtnAcceptComponent } from "../../../../../shared/components/btn-accept/btn-accept.component";
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-proveedor-popup',
   standalone: true,
-  imports: [],
+  imports: [InputFieldComponent, InputSelectComponent, BtnCloseComponent, BtnAcceptComponent, MatRadioModule, MatDialogModule, ReactiveFormsModule],
   templateUrl: './proveedor-popup.component.html',
   styleUrl: './proveedor-popup.component.scss'
 })
 export class ProveedorPopupComponent {
+    private sAuth = inject(AuthService)
+    private sSweetAlert = inject(SweealertService);
+    _MatDialgoRef = inject(MatDialogRef<ProveedorPopupComponent>)
+    _fb = inject(FormBuilder)
+    isEditMode: boolean;
+    loginForm: any;
+    roles!: IRole[];
+    imgUrl: any; //es una url que se pasa para visualiar la img,
+    fileToUpload: any;
+  
+    constructor(@Inject(MAT_DIALOG_DATA) public data: any,) {
+      this.isEditMode = !!data.payload;
+  
+      this.loginForm = this._fb.group({
+        firstname: [data.payload?.firstname || '', [Validators.required]],
+        lastname: [data.payload?.lastname || '', [Validators.required]],
+        phoneNumber: [data.payload?.phoneNumber || '', [Validators.required]],
+        empresa: [data.payload?.empresa || '', [Validators.required]],
+        status: [data.payload?.status || 'A', [Validators.required]],
+        address: [data.payload?.address || '', [Validators.required]],
+        website: [data.payload?.website || '', [Validators.required]],
+        account: [data.payload?.account || '', [Validators.required]],
+        nameaccount: [data.payload?.nameaccount || '', [Validators.required]],
+        namebanck: [data.payload?.namebanck || '', [Validators.required]],
+        profileImg: [null]
+      });
+      this.imgUrl = data.payload?.profile
+    }
+  
+    ngOnInit(): void {
+      this.getRole();
+    }
+  
+    
+    onSubmit() {
+      if (this.loginForm.valid) {
+        this.sSweetAlert.showLoading();
+        const formData = new FormData();
+          formData.append('PhoneNumber', this.loginForm.value.phoneNumber),
+          formData.append('Firstname', this.loginForm.value.firstname),
+          formData.append('Lastname', this.loginForm.value.lastname),
+          formData.append('Status', this.loginForm.value.status),
+          formData.append('NameEmpresa',this.loginForm.value.empresa),
+          formData.append('Address', this.loginForm.value.address),
+          formData.append('Website', this.loginForm.value.website),
+          formData.append('Account', this.loginForm.value.account)
+          formData.append('NameAccount', this.loginForm.value.nameaccount),
+          formData.append('NameBanck', this.loginForm.value.namebanck)
 
+        if (this.isEditMode) 
+          console.log('editar');
+        const idUser = this.data.payload.id;
+        if (idUser > 0) {
+          this.sAuth.modifyUser(formData, idUser).subscribe((data: IResult<string>) => {
+            if (data.isSuccess) {
+              this.sSweetAlert.closeLoading();
+              this._MatDialgoRef.close(true)
+            } else {
+              this.sSweetAlert.showError(data.error || 'Error  al editar el categoria')
+            }
+          })
+        }
+
+      } else {
+        if (this.isEditMode) 
+        console.log('crear categoria');
+        this.sAuth.register(FormData).subscribe((data: IResult<boolean>) => {
+          if (data.isSuccess) {
+            this.sSweetAlert.closeLoading();
+            this._MatDialgoRef.close(true)
+          } else {
+            this.sSweetAlert.showError(data.error || "Error al crear la categoria")
+          }
+   })
+
+}
+
+
+    }
+    getFormControl(controlName: string): FormControl | null {
+      const control = this.loginForm.get(controlName);
+      return control instanceof FormControl ? control : null;
+    }
+  
+    getRole() {
+      this.sAuth.getRoles().subscribe((data: IResult<IRole[]>) => {
+        if (data.isSuccess) {
+          this.roles = data.value!
+        }
+      })
+    }
+  
+  
+    onFileChange(event: any) {
+      const file = event.target.files[0];
+      this.fileToUpload = file;
+      this.loginForm.patchValue({
+        profileImg: file
+      });
+  
+      let reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imgUrl = e.target.result;
+      };
+  
+      reader.readAsDataURL(this.fileToUpload);
+    }
+    getImgProfile() {
+      return this.loginForm.value.profileImg
+    }
 }
--- src/app/features/catalogo/pages/proveedore/proveedore.component.ts
+++ src/app/features/catalogo/pages/proveedore/proveedore.component.ts
@@ -1,10 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+
 import { InputSearchComponent } from '../../../../shared/components/input-search/input-search.component';
 import { TitleService } from '../../../../core/services/title.service';
 import { MatDialog } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { RegisterProveedorComponent } from '../../components/modals/register-proveedor/register-proveedor.component';
+import { ProveedorPopupComponent } from '../../components/modals/proveedor-popup/proveedor-popup.component';
+import { SweealertService } from '../../../../core/services/sweealert.service';
+import { BaseApiService } from '../../../../core/services/base-api.service';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { ProveedorDetailPopupComponent } from '../../components/modals/proveedor-detail-popup/proveedor-detail-popup.component';
 
 export interface Proveedor {
   nombre: string;
@@ -17,7 +23,7 @@ export interface Proveedor {
 @Component({
   selector: 'app-proveedore',
   standalone: true,
-  imports: [InputSearchComponent, RegisterProveedorComponent, MatIconModule, MatTableModule],
+  imports: [InputSearchComponent,RegisterProveedorComponent, MatIconModule, MatIcon, RegisterProveedorComponent, MatSidenavModule,ProveedorDetailPopupComponent, MatTableModule],
   templateUrl: './proveedore.component.html',
   styleUrl: './proveedore.component.scss'
 })
@@ -27,6 +33,10 @@ export class ProveedoreComponent implements OnInit {
   _matDialog = inject(MatDialog)
   searchMessage = "Buscar proveedor"
   dataSource: any;
+  sSweetalert = inject(SweealertService);
+  sBaseApi = inject(BaseApiService);
+  selectedId: any;
+  @ViewChild('sidenav') sidenav!: MatSidenav
   onSearch(event: any) {
   }
 
@@ -34,19 +44,63 @@ export class ProveedoreComponent implements OnInit {
     this.sTitle.setTitle("proveedores")
     this.getAllProveedores()
   }
+  closeSidenav() {
+    this.sidenav.close();
+  }
 
   onOpenModal() {
+    this._matDialog.open(ProveedorPopupComponent, {
+      width: '60vw',
+      maxWidth: '60vw',
+      disableClose: true,
+      data: {}
+    }).afterClosed().subscribe((result) => {
+      if (result) {
+        this.getAllProveedores();
+      }
+    });
+    
   }
 
-  onDelete(id: any) {
-  }
+    onDelete(id: any) {
+      this.sSweetalert.showConfirmation(`Quieres eliminar al proveedor: ${id}`, () => {
+        this.sBaseApi.removeItem('Category', id).subscribe((data: any) => {
+          if (data.isSuccess) {
+            // this.sSweetalert.showConfirmation('Categoria eliminada',)
+          }
+        })
+        // this.selectedId.deleteUser(id).subscribe((data: IResult<string>) => {
+        //   if (data.isSuccess) {
+        //     this.sSweetalert.showSuccess('Categoria eliminada')
+        //   } else {
+        //     this.sSweetalert.showError("No se pudo eliminar la categoria")
+        //   }
+        // })
+      });
+    }
 
   onEdit(id: any) {
+    this._matDialog.open(ProveedorPopupComponent, {
+      width: '60vw',
+      maxWidth: '60vw',
+      disableClose: true,
+      data: { payload: id}
+    }).afterClosed().subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.getAllProveedores();
+      }
+    })
   }
 
-  onViewDetails(id: any) {
-
-  }
+  onViewDetails(id: Proveedor) {
+      this.selectedId = id;
+      if (this.sidenav) {
+        this.sidenav.open();
+      } else {
+        console.error('El sidenav no está definido.');
+      }
+    }
 
 
   getAllProveedores() {
