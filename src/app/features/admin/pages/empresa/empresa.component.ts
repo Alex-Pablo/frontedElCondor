c35b485fd916a5dfd424d5802e25674df553d602
Soporte Campo <campo@mcdcentral.local>
2024-10-18 00:16:53 -0600

    mis cambios

--- src/app/features/admin/pages/empresa/empresa.component.ts
+++ src/app/features/admin/pages/empresa/empresa.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-empresa',
-  standalone: true,
-  imports: [],
   templateUrl: './empresa.component.html',
-  styleUrl: './empresa.component.scss'
+  styleUrls: ['./empresa.component.scss'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class EmpresaComponent {
+  empresaForm: FormGroup;
+  logotipoURL: string | ArrayBuffer | null = null;
 
+  constructor(private fb: FormBuilder) {
+    this.empresaForm = this.fb.group({
+      nombre: [''],
+      numero: [''],
+      correo: [''],
+      direccion: ['']
+    });
+  }
+
+  onFileChange(event: any) {
+    const file = event.target.files;
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.logotipoURL = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onSubmit() {
+    console.log(this.empresaForm.value);
+  }
 }
