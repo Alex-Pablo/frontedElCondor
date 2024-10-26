ff5397784088aa9faeaf12d36825ac4f8ac0e27c
ajanels <cajanela1@miumg.edu.gt>
2024-10-25 19:58:19 -0600

    categotias

--- src/app/features/catalogo/components/modals/categoria-detail-popup/categoria-detail-popup.component.ts
+++ src/app/features/catalogo/components/modals/categoria-detail-popup/categoria-detail-popup.component.ts
@@ -1,8 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { Component, EventEmitter, Input, Output,inject } from '@angular/core';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from '../../../../../core/services/auth.service';
+import { ICategorias } from '../../../../../shared/models/Icategorias';
+
 
 @Component({
   selector: 'app-categoria-detail-popup',
@@ -14,6 +18,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 export class CategoriaDetailPopupComponent {
   @Input() id!: any;
   @Output() closeSidenav: EventEmitter<void> = new EventEmitter<void>();
+
+  categoriaDetail: ICategorias | undefined;
+
+ 
+  
+  
 
   public close() {
     console.log('sii');
